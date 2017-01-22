02da26b78409457a1c0085d14e22826af862ba13
Chris Cornutt <enygma@phpdeveloper.org>
2017-01-22 08:17:58 -0600

    updating main page template and the layout for bootstrap

--- templates/index/index.php
+++ templates/index/index.php
@@ -2,6 +2,8 @@
 
 {% block content %}
 
+<h3>Congratulations!</h3>
+
 The installation of the Slim skeleton application was successful! You can find this view in "templates/index/index.php".
 
 {% endblock %}
--- templates/layouts/main.php
+++ templates/layouts/main.php
@@ -1,4 +1,34 @@
-<h3>Congratulations!</h3>
-<p>
-{% block content %}{% endblock %}
-</p>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Sunshine PHP 2017</title>
+        <link href="/assets/css/bootstrap.min.css" rel="stylesheet"/>
+        <link href="/assets/css/site.css" rel="stylesheet"/>
+    </head>
+    <body>
+        <nav class="navbar navbar-inverse navbar-fixed-top mynav">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" style="color:#FFFFFF" href="#">SUNPHP17</a>
+                </div>
+                <div id="navbar" class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav">
+                    <li><a href="/">Home</a></li>
+                    </ul>
+                </div><!--/.nav-collapse -->
+            </div>
+        </nav>
+
+        <div class="container" style="padding-top:50px">
+        <p>
+        {% block content %}{% endblock %}
+        </p>
+        </div>
+    </body>
+</html>
