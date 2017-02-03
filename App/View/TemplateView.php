db4c0f02a08b9520190453f897981ebe8ff6cd53
Chris Cornutt <ccornutt@ccornutt-ltm.internal.salesforce.com>
2017-02-03 17:47:50 -0500

    adding the user and messages to the template view

--- App/View/TemplateView.php
+++ App/View/TemplateView.php
@@ -8,6 +8,13 @@ class TemplateView extends \Slim\Views\Twig
     public function render(ResponseInterface $response, $template, $data = [])
     {
         // This is where you'd load things for the entire template to use
+        $user = $this['container']->session->get('user');
+        if ($user !== null) {
+            $data['user'] = $user;
+        }
+
+        // Add any messages
+        $data['messages'] = $this['container']->message->get();
 
         parent::render($response, $template, $data);
     }
