46371340b27307cca27633c4d0f68e7c72545014
Chris Cornutt <enygma@phpdeveloper.org>
2017-02-04 08:36:21 -0500

    adding permission fetching and "has permission" methods to User model

--- App/Model/User.php
+++ App/Model/User.php
@@ -2,8 +2,30 @@
 
 namespace App\Model;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Capsule\Manager as DB;
+use App\Model\Permission;
 
 class User extends Model
 {
+    public function permissions()
+    {
+        $permissions = DB::table('users')
+            ->select('permissions.*')
+            ->join('users_permissions', 'users.id', 'users_permissions.user_id')
+            ->join('permissions', 'users_permissions.permission_id', 'permissions.id')
+            ->where('users.id', '=', $this->id)
+            ->get();
+        foreach ($permissions as $index => $record) {
+            $permissions[$index] = new \App\Model\Permission((array)$record);
+        }
+        return collect($permissions);
+    }
 
+    public function hasPermission($permissionName)
+    {
+        $result = $this->permissions()->first(function($value, $key) use ($permissionName) {
+            return $value->name == $permissionName;
+        });
+        return ($result == null);
+    }
 }
