a7fde034fd8c4223c41c76f74b14c025fa5acd59
Chris Cornutt <enygma@phpdeveloper.org>
2017-02-04 08:38:39 -0500

    adding more user routes, an error route and changing up the middleware

--- bootstrap/routes.php
+++ bootstrap/routes.php
@@ -1,6 +1,7 @@
 <?php
 
 $app->get('/', '\App\Controller\IndexController:index');
+$app->get('/error', '\App\Controller\IndexController:error');
 
 $app->group('/user', function() use ($app) {
     $app->get('', '\App\Controller\UserController:index');
@@ -11,8 +12,12 @@ $app->group('/user', function() use ($app) {
     $app->get('/reset', '\App\Controller\UserController:reset');
     $app->post('/reset', '\App\Controller\UserController:resetSubmit');
 
+    $app->get('/logout', '\App\Controller\UserController:logout');
+
     $app->group('/{id}', function() use ($app) {
         $app->get('/view', '\App\Controller\UserController:view');
 
-    })->add(new \App\Middleware\UserAccess($app->getContainer()));
+    })
+        ->add(new \App\Middleware\IsAuthed($app->getContainer()))
+        ->add(new \App\Middleware\UserAccess($app->getContainer()));
 });
